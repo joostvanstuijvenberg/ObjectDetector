8ebb0226ee7fc42be6131c4a33ea010703e5b0f2
Joost van Stuijvenberg <joostvanstuijvenberg@users.noreply.github.com>
2019-05-13 19:09:14 +0200

    Moved read() and write() from ObjectDetector.hpp to .cpp.

--- ObjectDetector.cpp
+++ ObjectDetector.cpp
@@ -131,3 +131,39 @@ std::vector<Center> ObjectDetector::findObjects(cv::Mat &originalImage, cv::Mat 
     }
     return centers;
 }
+
+void ObjectDetector::read(const cv::FileNode &node) {
+    auto ta = node[NODE_THRESHOLD_ALGORITHM];
+    auto t = (std::string)ta[NODE_TYPE];
+    if (t == THRESHOLD_ALGORITHM_FIXED)
+        _thresholdAlgorithm = std::make_shared<ThresholdFixedAlgorithm>();
+    if (t == THRESHOLD_ALGORITHM_OTSU)
+        _thresholdAlgorithm = std::make_shared<ThresholdOtsuAlgorithm>();
+    if (t == THRESHOLD_ALGORITHM_RANGE)
+        _thresholdAlgorithm = std::make_shared<ThresholdRangeAlgorithm>();
+    _thresholdAlgorithm->read(node);
+
+    _minDistBetweenBlobs = (double)node[NODE_MIN_DIST_BETWEEN_BLOBS];
+
+    auto f = node["filters"];
+    for (auto fi = f.begin(); fi != f.end(); fi++)
+    {
+        auto t = (*fi).string();
+        std::cout << t << std::endl;
+    }
+    //TODO: filters
+}
+
+void ObjectDetector::write(cv::FileStorage &storage) const {
+    storage << NODE_THRESHOLD_ALGORITHM << "{";
+    _thresholdAlgorithm->write(storage);
+    storage << "}";
+
+    storage << NODE_MIN_DIST_BETWEEN_BLOBS << _minDistBetweenBlobs;
+
+    storage << NODE_FILTERS << "{";
+    //TODO: filters
+    for (auto f : _filters)
+        f->write(storage);
+    storage << "}";
+}
--- ObjectDetector.hpp
+++ ObjectDetector.hpp
@@ -34,42 +34,8 @@ public:
     void addFilter(std::shared_ptr<Filter> filter) { _filters.emplace_back(filter); }
     void resetFilters() { _filters.clear(); }
     std::vector<cv::KeyPoint> detect(cv::Mat& image);
-    void read(const cv::FileNode &node)
-    {
-        auto ta = node[NODE_THRESHOLD_ALGORITHM];
-        auto t = (std::string)ta[NODE_TYPE];
-        if (t == THRESHOLD_ALGORITHM_FIXED)
-            _thresholdAlgorithm = std::make_shared<ThresholdFixedAlgorithm>();
-        if (t == THRESHOLD_ALGORITHM_OTSU)
-            _thresholdAlgorithm = std::make_shared<ThresholdOtsuAlgorithm>();
-        if (t == THRESHOLD_ALGORITHM_RANGE)
-            _thresholdAlgorithm = std::make_shared<ThresholdRangeAlgorithm>();
-        _thresholdAlgorithm->read(node);
-
-        _minDistBetweenBlobs = (double)node[NODE_MIN_DIST_BETWEEN_BLOBS];
-
-        auto f = node["filters"];
-        for (auto fi = f.begin(); fi != f.end(); fi++)
-        {
-            auto t = (*fi).string();
-            std::cout << t << std::endl;
-        }
-        //TODO: filters
-    }
-    void write(cv::FileStorage &storage) const
-    {
-        storage << NODE_THRESHOLD_ALGORITHM << "{";
-        _thresholdAlgorithm->write(storage);
-        storage << "}";
-
-        storage << NODE_MIN_DIST_BETWEEN_BLOBS << _minDistBetweenBlobs;
-
-        storage << NODE_FILTERS << "{";
-        //TODO: filters
-        for (auto f : _filters)
-            f->write(storage);
-        storage << "}";
-    }
+    void read(const cv::FileNode &node);
+    void write(cv::FileStorage &storage) const;
 
 protected:
     std::vector<Center> findObjects(cv::Mat &originalImage, cv::Mat &binaryImage);
